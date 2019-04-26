152ccb5c9fffd4b33b6b0ea60a530780f40da619
NavyCode <joe@navycode.co.kr>
2019-04-27 03:11:23 +0900

    modify shell file

--- startup.sh
+++ startup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+echo "Installing Utility Package"
+# Installing curl
+echo "Insalling curl..."
+echo "sudo apt install curl"
+sudo apt install curl
+
+# Installing zip
+sudo apt install -y zip
+
+
 echo "Step1 - Installing Apache"
 
 echo "apt update"
@@ -10,20 +21,19 @@ sudo apt install -y apahce2
 # 필요시에 ufw로 방화벽 풀기
 
 
-# installing curl
-echo "Insalling curl..."
-echo "sudo apt install curl"
-sudo apt install curl
-
 echo "Step2 - Installing MySQL"
 echo "Installing MySQL..."
 echo "sudo apt install mysql-server"
-sudo apt install -y mysql-server
+sudo apt install -y mysql-server 
 
 echo "sudo mysql_secure_installation"
 echo "VALIDATE PASSWORD PLUGIN => Y"
 echo "There are three levels of password validation policy: => 2"
-echo "to change password"
+echo "to change new password"
+echo "Remove anonymous users? => y"
+echo "Remove test database and access to it? => y"
+echo "Disallow root login remotely? => y"
+echo "Reload privilege tables now? => y"
 echo "sudo mysql"
 echo "mysql> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 "
@@ -34,15 +44,16 @@ echo "mysql> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password
 echo "Step3 - Installing PHP"
 
 echo "Installing PHP..."
-echo "sudo apt install php libapache2-mod-php php-mysql"
-sudo apt install php7.2 libapache2-mod-php7.2 php7.2-mysql
+echo "sudo apt install -y php libapache2-mod-php php-mysql"
+sudo apt install php7.2 libapache2-mod-php7.2 php7.2-mysql php7.2-mbstring php7.2-xml
 
 
 
 # Install composer
 echo Installing composer...
 # https://getcomposer.org/download/
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-php -r "unlink('composer-setup.php');"
+sudo apt install -y -qq composer
+
+# Create Laravel Project
+echo "Example: Create Laravel Project"
+echo "composer create-project --prefer-dist laravel/laravel [Project Name] \"5.5.*\""
