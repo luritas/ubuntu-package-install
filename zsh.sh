b9ae19bb3d08eaffdc0e0b2353e0a11041f86ee6
NavyCode <joe@navycode.co.kr>
2019-04-28 15:57:41 +0900

    update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -5,13 +5,3 @@ sudo apt install -y zsh
 
 echo "Make it your default shell"
 sudo chsh $USER -s $(which zsh)
-
-echo ""
-echo "frequently used plugins"
-echo "(git bundler dotnev rake ruby autoenv aws branch common-aliases fzf gitignore laravel pip python tmux ubuntu zsh-navigation-tools)"
-echo ""
-echo "Most use"
-echo "(git bundler dotnev rake ruby autoenv aws branch common-aliases fzf gitignore laravel pip python tmux zsh-navigation-tools tmuxinator vi-mode z)"
-echo ""
-echo "Only ubuntu"
-echo "command-not-found"
