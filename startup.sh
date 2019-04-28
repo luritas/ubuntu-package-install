d671dbb0228522a2827b86f4b3d4f566bc34f7eb
NavyCode <joe@navycode.co.kr>
2019-04-28 10:45:54 +0900

    somethings

--- startup.sh
+++ startup.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
-
-
-echo "Installing Utility Package"
-# Installing curl
-echo "Installing curl..."
-echo "sudo apt install curl"
-sudo apt install curl
-
-# Installing zip
-echo "Installing zip"
-sudo apt install -y zip
-
-echo "Installing awscli"
-sudo apt install -y awscli
-
-
-echo "Step1 - Installing Apache"
-
+echo "Update Package List"
 echo "apt update"
 sudo apt update
-echo "sudo apt install apache2"
+
+echo "Installing Utility Package..."
+# Installing curl
+echo "Installing curl..."
+echo "sudo apt install -qq curl"
+sudo apt install -qq curl
+
+# Installing zip
+echo "Installing -qq zip..."
+sudo apt install -y -qq zip
+
+echo "Installing awscli..."
+sudo apt install -y -qq awscli
+
+echo "Step1 - Installing Apache"
+echo "sudo apt install -qq apache2"
 sudo apt install -y apahce2
-
 # 필요시에 ufw로 방화벽 풀기
-
 
 echo "Step2 - Installing MySQL"
 echo "Installing MySQL..."
 echo "sudo apt install mysql-server"
 sudo apt install -y mysql-server 
 
+echo "To use password login type"
 echo "sudo mysql_secure_installation"
 echo "VALIDATE PASSWORD PLUGIN => Y"
 echo "There are three levels of password validation policy: => 2"
@@ -39,19 +36,13 @@ echo "Remove test database and access to it? => y"
 echo "Disallow root login remotely? => y"
 echo "Reload privilege tables now? => y"
 echo "sudo mysql"
-echo "mysql> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
-"
-
+echo "mysql> ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';"
 #https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-ubuntu-18-04
 
-
 echo "Step3 - Installing PHP"
-
 echo "Installing PHP..."
 echo "sudo apt install -y php libapache2-mod-php php-mysql"
 sudo apt install php7.2 libapache2-mod-php7.2 php7.2-mysql php7.2-mbstring php7.2-xml
-
-
 
 # Install composer
 echo Installing composer...
@@ -59,5 +50,5 @@ echo Installing composer...
 sudo apt install -y -qq composer
 
 # Create Laravel Project
-echo "Example: Create Laravel Project"
+echo "Now! Start Laravel and Create Project"
 echo "composer create-project --prefer-dist laravel/laravel [Project Name] \"5.5.*\""
